ce3ee61facdf0d64f034ae3fb60d40b288a4cf7a
Roger Fernandez <nietz.dren@gmail.com>
2016-03-28 22:10:47 -0300

    Adicionando flask application

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='PromocaoApp',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Promocoes',
+      author='Roger Fernandez',
+      author_email='',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+#      install_requires=['Flask==0.7.2', 'MarkupSafe' , 'Flask-SQLAlchemy==0.16'],
      )
