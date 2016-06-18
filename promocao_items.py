beaddff9608fbd53633c9d283720d5dfe674bb85
Roger Fernandez <nietz.dren@gmail.com>
2016-06-18 15:21:58 -0300

    atualizado captura de imagens otimizadas

--- promocao_items.py
+++ promocao_items.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from PIL import Image
+import urllib
 import pymongo
 import os
 import scrapinghub_funcs
@@ -11,6 +13,7 @@ db = client.promocao
 # LOCAL TEST
 #client = MongoClient()
 #db = client.promocoes
+#db = client.promocao
 
 items_collection = db.items
 #result = items_collection.create_index([('cod_prom', pymongo.ASCENDING)], unique=True)
@@ -22,8 +25,42 @@ for j in jobList:
 		items = scrapinghub_funcs.getItems(j['key'])
 		for i in items:
 			try:
+				url_image = 'http://promocao-rogerdev.rhcloud.com/static/images/imagenotfound.png'
+				try:
+					img = urllib.urlopen(i['url_img'])
+
+					#verificando se a url funciona
+					if(img.getcode() == 200):
+
+						# nomeando e capturando dados dos links
+						name = i['url_img'].split('/')[-1]
+						local_img = open(os.environ['OPENSHIFT_REPO_DIR'] + 'imgs/' + name, 'w')
+						local_img.write(img.read())
+
+						#fechando dados pegos da url
+						img.close()
+						local_img.close()
+
+						#tratando a imagem diminindo para 200x200 px
+						im = Image.open(os.environ['OPENSHIFT_REPO_DIR'] + 'imgs/' + name)
+						im.thumbnail((200,200))
+						im.save(os.environ['OPENSHIFT_REPO_DIR'] + 'imgs/mini_' + name)
+						im.close()
+
+						url_image = 'http://promocao-rogerdev.rhcloud.com/static/imgs/mini_' + name
+
+						#removendo antiga imagem
+						os.remove(os.environ['OPENSHIFT_REPO_DIR'] + 'imgs/' + name)
+
+				except:
+					pass
+				
+				i['url_img'] = url_image
+				
 				result = items_collection.insert_one(i)
+
 				print "inserindo dados no banco de dados job: " + j['key']
+
 			except:
 				print "Um ou mais items ja existiam na base ou ocorreu falha job: " + j['key'] + " item: " + i['cod_prom'][0]
 
