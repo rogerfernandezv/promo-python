0f6aa233de11382a8608b1aa839297380edff05b
Roger Fernandez <nietz.dren@gmail.com>
2016-07-03 20:55:23 -0300

    Diminindo tempo de execucao do script

--- .openshift/cron/minutely/ex_prom_items.sh
+++ .openshift/cron/minutely/ex_prom_items.sh
@@ -3,6 +3,6 @@
 minute=$(date +%M)
 #minute2=echo $minute | sed -e 's/^0//'
 
-if [ $(($minute % 5)) -eq 0 ]; then
+if [ $(($minute % 3)) -eq 0 ]; then
 	python $OPENSHIFT_REPO_DIR/promocao_items.py
 fi
